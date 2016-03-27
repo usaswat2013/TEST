5ccdc5800af2db8ad6c00cf9114cc3819427870f
usaswat2013 <usaswat2013@gmail.com>
2016-03-27 03:18:35 +0000

    Modified version

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,16 +1,37 @@
 class ArticlesController < ApplicationController
   
+  def index
+    
+    @articles = Article.all
+    
+    
+  end
+  
   def new
   
     @article = Article.new
     
   end
   
+def edit
+  @article = Article.find(params[:id])
+end
+
+def update
+  @article = Article.find(params[:id])
+  if @article.update(article_params)
+    redirect_to @article
+  else
+    render 'edit'
+  end
+end
+  
   def show
     @article = Article.find(params[:id])
     
     
   end
+  
   def create 
     #render plain: params[:article].inspect
     @article = Article.new(article_params)
@@ -29,7 +50,9 @@ class ArticlesController < ApplicationController
     #redirect_to_ articles_show(@article)
   end
   
-  private 
+
+private 
+
     def article_params
       params.require(:article).permit(:title, :description)
     end
