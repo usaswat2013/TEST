1632e330167209cd00df1fd0921f6ee8edde54b7
usaswat2013 <usaswat2013@gmail.com>
2016-03-27 03:39:23 +0000

    New features

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -32,6 +32,16 @@ end
     
   end
   
+  def destroy
+    
+    @article = Article.find(params[:id])
+    @article.destroy
+    
+    flash[:notice] = 'Article was successfully deleted'
+    redirect_to articles_path(@article)
+    
+  end
+  
   def create 
     #render plain: params[:article].inspect
     @article = Article.new(article_params)
